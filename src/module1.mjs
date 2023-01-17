3861f5b369acd94dd6195184e82beecc4ea67640
Princekumar-Mehta <2001.mehta.2001@gmail.com>
2023-01-17 23:37:47 +0530

    module default named import export mjs

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+const a="Prince";
+const b="Maurya";
+const c="Malhar";
+
+export default a;
+export {b,c};
---
+++ src/module2.mjs
@@ -0,0 +1,5 @@
+import xyz,{b,c} from "./module1.mjs";
+
+console.log(xyz);
+console.log(b);
+console.log(c);
