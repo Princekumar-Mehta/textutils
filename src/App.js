1ffcedd41f3edf659bf5b350b92d9277c2141033
Princekumar-Mehta <2001.mehta.2001@gmail.com>
2023-01-28 21:15:50 +0530

    alert message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-//import Form from "./components/Form";
+import Alert from "./components/Alert";
+import Form from "./components/Form";
 import { useState } from "react";
 import About from "./components/About";
 function App() {
   const [theme, setTheme] = useState("dark");
+  const [alert, setAlert] = useState(null);
+  const showAlert = function(message,type){
+     setAlert({
+      msg:message,
+      type:type
+     });
+     setTimeout(() => {
+      setAlert(null);
+     }, 1000);
+  }
   const toggle = function () {
     if (theme === "dark") {
       setTheme("light");
-    } else setTheme("dark");
+      showAlert("Light Theme Enabled","success");
+    } else {
+      setTheme("dark");
+      showAlert("Dark Theme Enabled","success");
+    }
   };
   return (
     <>
@@ -19,8 +34,9 @@ function App() {
           theme={theme}
           toggle={toggle}
         ></Navbar>
+        <Alert alert={alert}/>
         <div className="container mb-8">
-          {/*<Form heading="Enter text here"></Form>*/}
+          <Form showAlert={showAlert} heading="Enter text here"></Form>
           <About theme={theme} toggle={toggle}></About>
         </div>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -7,9 +7,18 @@ export default function Form(props) {
   };
   const handleUpClick = () => {
     setText(text.toUpperCase());
+    props.showAlert("Converted to Upper case", "success");
   };
   const handleLowerClick = () => {
     setText(text.toLowerCase());
+    props.showAlert("Converted to Lower case", "success");
+  };
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to clipboard", "success");
   };
   const [text, setText] = useState("");
   return (
@@ -20,6 +29,7 @@ export default function Form(props) {
           <textarea
             className="htmlForm-control"
             rows="5"
+            id="myBox"
             onChange={handleOnChange}
             value={text}
           />
@@ -29,6 +39,9 @@ export default function Form(props) {
         </button>
         <button className="btn btn-primary mx-2" onClick={handleLowerClick}>
           Convert to lower case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy
         </button>
       </div>
       <div className="container my-3">
